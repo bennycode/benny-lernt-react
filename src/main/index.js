c958f4ef714359a9b3277092b5ce5a3f25001ad0
Benny Neugebauer <bn@bennyn.de>
2018-06-23 20:22:13 +0200

    Using webpack to babel JSX code which then can be imported in HTML.
    Dependency resolution:
    1. webpack
    2. babel-loader
    3. babel-core
    4. babel-preset-react

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,6 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-const element = App;
 const container = document.getElementById('app');
-
-ReactDOM.render(element, container);
+ReactDOM.render(<App/>, container);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const pkg = require('./package.json');
+
+module.exports = {
+  entry: {
+    [pkg.name]: `${__dirname}/${pkg.main}`,
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-react']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
