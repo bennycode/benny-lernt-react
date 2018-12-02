a5db7b1adb51a98b128ee66a1fbd084305f54386
Benny Neugebauer <bn@bennyn.de>
2018-12-02 19:15:53 +0100

    test: Add API docs test

--- src/main/Server.test.ts
+++ src/main/Server.test.ts
@@ -1,18 +1,36 @@
 import Server from './Server';
+import * as http from "http";
 import request = require('supertest');
 
+const failOnError = (error: Error) => (error ? fail(error) : undefined);
+
 describe('Server', () => {
-  it('serves a root site', async (done) => {
-    const server = new Server();
-    await server.start(3001);
-    request(server['server']!.listener)
-      .get('/')
-      .expect(200, (error) => {
-        if (error) {
-          done.fail(error);
-        } else {
-          done();
-        }
-      });
+  describe('Routing', () => {
+    let server: Server;
+    let listener: http.Server;
+
+    beforeEach(async function () {
+      server = new Server();
+      await server.start(3001);
+      listener = server['server']!.listener;
+    });
+
+    afterEach(async () => {
+      if (server) {
+        await server.stop();
+      }
+    });
+
+    it('serves a root site', async () => {
+      request(listener)
+        .get('/')
+        .expect(200, failOnError);
+    });
+
+    it('serves an API documentation', async () => {
+      request(listener)
+        .get('/documentation')
+        .expect(200, failOnError);
+    });
   });
 });
