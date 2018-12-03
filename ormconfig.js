b8e8a649273f4dabbf018ebd02672100385a2dba
Benny Neugebauer <bn@bennyn.de>
2018-12-03 21:20:33 +0100

    feat: Add ORM

---
+++ ormconfig.js
@@ -0,0 +1,24 @@
+const ConnectionOptions = {
+  cli: {
+    entitiesDir: 'src/main/entity',
+    migrationsDir: 'src/migration',
+    subscribersDir: 'src/subscriber',
+  },
+  database: 'database/development.db3',
+  entities: ['src/main/entity/*.ts'],
+  logging: false,
+  migrations: ['src/migration/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  synchronize: true,
+  type: 'sqlite',
+};
+
+if (process.env.NODE_ENV === 'production') {
+  ConnectionOptions.database = 'database/production.db3';
+}
+
+if (process.env.NODE_ENV === 'test') {
+  ConnectionOptions.database = 'database/test.db3';
+}
+
+module.exports = ConnectionOptions;
--- src/main/Server.ts
+++ src/main/Server.ts
@@ -1,8 +1,12 @@
+import {createConnection, getManager} from 'typeorm';
+import * as Boom from 'boom';
 import * as Hapi from 'hapi';
 import * as Inert from 'inert';
+import * as Joi from 'joi';
 import * as logdown from 'logdown';
 import * as path from 'path';
 import * as Vision from 'vision';
+import {Animal} from './entity/Animal';
 
 class Server {
   private readonly logger = logdown('prefix:Server', {
@@ -55,11 +59,33 @@ class Server {
           },
           tags: ['api']
         },
+      },
+      {
+        method: 'GET',
+        path: '/rest/animals/{id}',
+        options: {
+          handler: async function animalGetByIdAction(request) {
+            const animalRepository = getManager().getRepository(Animal);
+            const animal = await animalRepository.findOne(request.params.id);
+
+            if (!animal) {
+              return Boom.notFound();
+            }
+
+            return animal;
+          },
+          validate: {
+            params: {
+              id: Joi.number().required(),
+            },
+          },
+        },
       }
     ]);
   }
 
   async start(port: number = 3000): Promise<void> {
+    await createConnection();
     this.server = new Hapi.Server({
       port,
       routes: {
