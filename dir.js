54fec7a5692dfd2cb452e413c7fca5edeafce122
Benny Neugebauer <bn@bennyn.de>
2018-12-04 23:21:12 +0100

    refactor: Move "src/frontend" to "src/view"

--- dir.js
+++ dir.js
@@ -5,9 +5,9 @@ const dir = {};
 dir.data = path.join(__dirname, 'data');
 
 dir.dist = path.join(__dirname, 'dist');
-dir.distView = path.join(dir.dist, 'frontend');
+dir.distView = path.join(dir.dist, 'view');
 
 dir.src = path.resolve(tsConfig.compilerOptions.rootDir);
-dir.srcView = path.join(dir.src, 'frontend');
+dir.srcView = path.join(dir.src, 'view');
 
 module.exports = dir;
