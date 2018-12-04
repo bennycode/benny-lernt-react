4dd8540e32a6c36c7c6e8a7ce2fad6c0445d66bf
Benny Neugebauer <bn@bennyn.de>
2018-12-04 22:53:18 +0100

    chore: Strategy to move "main" directory

--- dir.js
+++ dir.js
@@ -1,3 +1,4 @@
+const tsConfig = require('./tsconfig');
 const path = require('path');
 
 const dir = {};
@@ -6,7 +7,7 @@ dir.data = path.join(__dirname, 'data');
 dir.dist = path.join(__dirname, 'dist');
 dir.distView = path.join(dir.dist, 'frontend');
 
-dir.src = path.join(__dirname, 'src', 'main');
+dir.src = path.resolve(tsConfig.compilerOptions.rootDir);
 dir.srcView = path.join(dir.src, 'frontend');
 
 module.exports = dir;
