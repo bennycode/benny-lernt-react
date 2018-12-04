76cc31564a08b90f41e22474743d618f26aba8c6
Benny Neugebauer <bn@bennyn.de>
2018-12-04 23:10:03 +0100

    fix test

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ const gulp = require('gulp');
 
 const setupEnvironment = require('./gulp/setupEnvironment');
 
-const jasminConfig = require('./jasmine');
 const nodemonConfig = require('./nodemon');
 const tsConfig = require('./tsconfig');
 const webpackConfig = require('./webpack.config');
@@ -61,7 +60,8 @@ gulp.task('dev', async done => {
 });
 
 gulp.task('test:backend', (done) => {
-  require('./gulp/test/backend')(jasminConfig, done);
+  const config = path.join(__dirname, 'jasmine.json');
+  require('./gulp/test/backend')(config, done);
 });
 
 gulp.task('test', gulp.series('test:backend'));
