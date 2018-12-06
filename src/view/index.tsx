2d8151780376d8386e39dc0b237491d002ce0f3e
Benny Neugebauer <bn@bennyn.de>
2018-12-06 21:13:46 +0100

    feat: Show headline

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,6 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import OrderedList from './list/OrderedList';
+import Typography from '@material-ui/core/Typography';
 
 const root = document.getElementById('root');
-ReactDOM.render(<OrderedList/>, root);
+ReactDOM.render(
+  <React.Fragment>
+    <Typography component="h2" variant="headline">
+      Animals
+    </Typography>
+    <OrderedList/>
+  </React.Fragment>
+  , root);
