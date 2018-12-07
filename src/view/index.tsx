5a0fe95f0ef779d999b4cf4f94c4053377721415
Benny Neugebauer <bn@bennyn.de>
2018-12-07 13:49:15 +0100

    feat: Create Animal button

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import OrderedList from './list/OrderedList';
-import Typography from '@material-ui/core/Typography';
+import {Button, Typography} from '@material-ui/core';
 
 const root = document.getElementById('root');
 ReactDOM.render(
@@ -9,6 +9,9 @@ ReactDOM.render(
     <Typography component="h2" variant="headline">
       Animals
     </Typography>
+    <Button color="primary" variant="outlined">
+      <a href={'/documentation'} style={{textDecoration: 'none'}} target={'_blank'} rel='noopener'>Create animal</a>
+    </Button>
     <OrderedList/>
   </React.Fragment>
   , root);
