c6ad2422486a68059d93cb3b08263b4c9c808b41
Benny Neugebauer <bn@bennyn.de>
2018-12-11 13:07:12 +0100

    Add "PermanentDrawerLeft"

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Dashboard from './Dashboard';
-import OrderedList from './list/OrderedList';
+import PermanentDrawerLeft from "./PermanentDrawerLeft";
 
 const root = document.getElementById('root');
 ReactDOM.render(
   <Dashboard>
-    <OrderedList/>
+    <PermanentDrawerLeft />
   </Dashboard>, root);
